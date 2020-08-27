1ce8d7603749ccb07d952b4534d465fe39ea3165
aii-jayson <63378546+aii-jayson@users.noreply.github.com>
2020-08-27 12:07:18 +0900

    fix dataset creator

--- src/scrabble_test.py
+++ src/scrabble_test.py
@@ -3,7 +3,7 @@
 
 import os
 import random
-
+import json
 import gin
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,7 +39,8 @@ def get_shared_specs(epochs, batch_size, latent_dim, embed_y, num_gen, kernel_re
 
 
 @gin.configurable('io')
-def setup_io(base_path, checkpoint_dir, gen_imgs_dir, gen_data_dir, model_dir, raw_dir, read_dir, input_dim, buf_size, n_classes,
+def setup_io(base_path, checkpoint_dir, gen_imgs_dir, gen_data_dir, model_dir, raw_dir, read_dir, input_dim, buf_size,
+             n_classes,
              seq_len, char_vec, bucket_size):
     gen_path = base_path + gen_imgs_dir
     gen_data_pth = base_path + gen_data_dir
@@ -47,12 +48,13 @@ def setup_io(base_path, checkpoint_dir, gen_imgs_dir, gen_data_dir, model_dir, r
     m_path = base_path + model_dir
     raw_dir = base_path + raw_dir
     read_dir = base_path + read_dir
-    return input_dim, buf_size, n_classes, seq_len, bucket_size, ckpt_path, gen_path,gen_data_pth, m_path, raw_dir, read_dir, char_vec
+    return input_dim, buf_size, n_classes, seq_len, bucket_size, ckpt_path, gen_path, gen_data_pth, m_path, raw_dir, read_dir, char_vec
 
 
 def main():
     # init params
-    gin.parse_config_file('/root/scrabble-gan/src/scrabble_gan.gin') #'/Users/jsoncunanan/Documents/GitHub/scrabble-gan/src/scrabble_gan.gin'
+    gin.parse_config_file(
+        '/Users/jsoncunanan/Documents/GitHub/scrabble-gan/src/scrabble_gan.gin')  # '/root/scrabble-gan/src/scrabble_gan.gin'
     epochs, batch_size, latent_dim, embed_y, num_gen, kernel_reg, g_bw_attention, d_bw_attention = get_shared_specs()
     in_dim, buf_size, n_classes, seq_len, bucket_size, ckpt_path, gen_path, gen_data_path, m_path, raw_dir, read_dir, char_vec = setup_io()
 
@@ -61,11 +63,10 @@ def main():
         print('converting iamDB-Dataset to GAN format...')
         init_reading(raw_dir, read_dir, in_dim, bucket_size)
 
-
     # load random words into memory (used for word generation by G)
     random_words = load_random_word_list(read_dir, bucket_size, char_vec)
 
-    rnd_wrd = [char_vec.index(char) for char in 'withaMac'] #
+    rnd_wrd = [char_vec.index(char) for char in 'withaMac']  #
     # load and preprocess dataset (python generator)
 
     # init generator, discriminator and recognizer
@@ -74,10 +75,9 @@ def main():
 
     seed = tf.random.normal([num_gen, latent_dim], seed=711)
 
-
     random_bucket_idx = random.randint(4, bucket_size - 1)
-    #single preds
-    #particular styles
+    # single preds
+    # particular styles
     # style_1 = [seed[i, :] for i in [7,9,10,20,21, 25, 34, 41]]#[0,2,6,7,9,12,16,19]
     # style_1 = tf.convert_to_tensor(style_1)
     # labels = np.array([rnd_wrd for _ in range(1)], np.int32)
@@ -85,7 +85,7 @@ def main():
     # style_1 = tf.convert_to_tensor(style_1)
     # test_input = [style_1, labels]
 
-    #multiple preds
+    # multiple preds
     labels = np.array([random.choice(random_words[random_bucket_idx]) for _ in range(num_gen)], np.int32)
     test_input = [seed, labels]
 
@@ -95,29 +95,36 @@ def main():
     if not os.path.exists(gen_data_path):
         os.makedirs(gen_data_path)
 
-    def make_dataset(num_gen, num_batch=2):
+    def make_dataset(num_gen, num_batch=20):
         lines = []
-        for _ in trange(num_batch):
+        dic = {}
+        for j in trange(num_batch):
 
             seed = tf.random.normal([num_gen, latent_dim])
             random_bucket_idx = random.randint(4, bucket_size - 1)
-            labels = np.array([random.choice(random_words[random_bucket_idx]) for _ in range(num_gen)], np.int32)
-            test_input = [seed, labels]
+            labels = np.array([random.choice(random_words[random_bucket_idx]) for _ in range(20)], np.int32)
+            test_input = [seed[:20], labels]
 
             preds = generator(test_input, training=False)
             preds = (preds + 1) / 2.0
 
-            for i in trange(preds.shape[0]):
-                filename = str("".join([char_vec[label] for label in labels[i]]))
+            for i in range(preds.shape[0]):
+                name = str("".join([char_vec[label] for label in labels[i]]))
+                filename = str(j) + '_' + str(i)
+                # save label to json file
+                dic["answer"] = name
+                with open(gen_data_path + filename+'.json', 'w') as outfile:
+                    json.dump(dic, outfile, indent=3)
                 tf.keras.preprocessing.image.save_img(gen_data_path + filename + ".png", preds[i])
-                lines.append( 'test/' + filename + '.png\t' + filename + '\n')
-
-        anno = open(gen_data_path+"gt.txt", "w")
+                lines.append('test/' + filename + '.png\t' + name + '\n')
+        # for lmdb use
+        anno = open(gen_data_path + "gt.txt", "w")
         anno.writelines(lines)
         anno.close()
 
     make_dataset(num_gen)
     # print(predictions.numpy()[0].shape)
+
 
 if __name__ == "__main__":
     main()
